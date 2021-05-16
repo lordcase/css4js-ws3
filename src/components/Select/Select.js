82b5c1dc0cb827cb445707a412b0b83746455c05
Krisztian Dobo <lordcase@gmail.com>
2021-05-16 07:38:15 +0200

    feat(select): done

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <IconWrapper>
+        <Icon id="chevron-down" size="20" />
+      </IconWrapper>
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+    </SelectWrapper>
   );
 };
 
 export default Select;
+
+const SelectWrapper = styled.div`
+  select {
+    appearance: none;
+    background: none;
+    border: none;
+    padding: 12px;
+    padding-right: 52px;
+    font-size: 16px;
+    font-weight: 400;
+    color: ${COLORS.gray700};
+    position: relative;
+    &:hover {
+      color: ${COLORS.black};
+    }
+  }
+  position: relative;
+  border: 0;
+  display: inline-block;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  position: absolute;
+  right: 16px;
+  top: 12px;
+  color: ${COLORS.gray700};
+  ${SelectWrapper}:hover & {
+    color: ${COLORS.black};
+  }
+`;
